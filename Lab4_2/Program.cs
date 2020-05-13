c6e0a75975a890d1842e8b3c5b8c3e24f6a977bc
Hrystyna18 <tymkivhrystyna007@icloud.com>
2020-05-13 21:04:55 +0300

    Add project files.

---
+++ Lab4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        public static Student[] group = new Student[int.Parse(Console.ReadLine())];
+        private static void Main(string[] args)
+        {
+            Output.Write(Input.Read());
+        }
+    }
+}
---
+++ Lab4_2/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lab4_2
+{
+    class Student
+    {
+        private DateTime _birthday;
+        private string _lastname;
+        private string _initials;
+
+            public DateTime Birthday
+        {
+            get => _birthday;
+            set => _birthday = value;
+        }
+               public string Lastname
+        {
+            get => _lastname;
+            set => _lastname = value;
+        }
+        public string Initials
+        {
+            get => _initials;
+            set => _initials = value;
+        }
+       
+        public Student()
+        {
+            Birthday = DateTime.Parse("01.01.01");
+            Lastname = "Не вказали";
+            Initials = "Не вказали";
+        }
+    }
+}
